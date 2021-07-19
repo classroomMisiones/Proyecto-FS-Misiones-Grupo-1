1b371c00c87a9027e79213c66c381e960f184758
Mariano Alvez <alvez_jmariano@hotmail.com>
2021-07-19 14:47:00 -0300

    funciona componente registro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RegistrationFormComponent } from './usuario/registration-form/registration-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationFormComponent
   ],
   imports: [
     BrowserModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
+  
 })
+
 export class AppModule { }
