e9f6f143bb7df8a7ad7c52e911b1e319aed660e2
Mariano Alvez <alvez_jmariano@hotmail.com>
2021-07-21 11:46:12 -0300

    componente login creado ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './usuario/login/login.component';
 import { RegistrationFormComponent } from './usuario/registration-form/registration-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistrationFormComponent
+    RegistrationFormComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
