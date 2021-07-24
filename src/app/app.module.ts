d7fb27609fbd0dc58372c27978a38acd466948e8
exequieldev93 <exequiel191993@gmail.com>
2021-07-24 13:51:04 -0300

    merge rama develop realizado y funcional

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-Mariano_feature_formulario.registro
 import { LoginComponent } from './usuario/login/login.component';
 import { RegistrationFormComponent } from './usuario/registration-form/registration-form.component';
 
 import { VenderCriptoMonedaComponent } from './CriptoMoneda/vender-cripto-moneda/vender-cripto-moneda.component';
 import { ModuloPesosComponent } from './Inicio/modulo-pesos/modulo-pesos.component';
-develop
+
 
 @NgModule({
   declarations: [
