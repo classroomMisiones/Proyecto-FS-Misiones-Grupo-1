407a7a7ef36c79635e7ccd4adf9ab5f205771eb9
Maximiliano Gamón <maximilianogamon@gmail.com>
2021-09-14 16:36:44 -0300

    Validaciones con Angular en el registro.
    Hay una cosa secretamente pendiente que Maxi no quiere que la profe Yamil se entere aún, ya la va a arreglar (?).

--- src/app/usuario/registration-form/registration-form.component.ts
+++ src/app/usuario/registration-form/registration-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-registration-form',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationFormComponent implements OnInit {
 
-  constructor() { }
+  registro: FormGroup = new FormGroup({});
+    
+
+  constructor(private fb: FormBuilder) {
+
+    this.registro = fb.group({
+    usuario: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    contrasena: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    repetirContrasena: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    nombre: new FormControl('', [Validators.required, Validators.pattern('^[A-Za-zñÑáéíóúÁÉÍÓÚ ]+$'), Validators.minLength(2)]),
+    apellido: new FormControl('', [Validators.required, Validators.pattern('^[A-Za-zñÑáéíóúÁÉÍÓÚ ]+$'), Validators.minLength(2)])}, 
+    )
+   }
 
   ngOnInit(): void {
   }
 
+  get usuario() {
+    return this.registro.get('usuario');
+    }
+  get email() {
+    return this.registro.get('email');
+    }
+  get contrasena() {
+    return this.registro.get('contrasena');
+    }
+  get repetirContrasena() {
+    return this.registro.get('repetirContrasena');
+    }
+  get nombre() {
+    return this.registro.get('nombre');
+    }
+  get apellido() {
+    return this.registro.get('apellido');
+    }
 }
