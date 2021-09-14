8a84f06642f0fef85c33109cb3307258064b599f
Maximiliano Gamón <maximilianogamon@gmail.com>
2021-09-14 17:50:12 -0300

    Validaciones en el login.

--- src/app/usuario/login/login.component.ts
+++ src/app/usuario/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  login = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    contrasena: new FormControl('', [Validators.required, Validators.minLength(5)])
+  })
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  get email() {
+    return this.login.get('email')
+  }
+  get contrasena() {
+    return this.login.get('contrasena')
   }
 
 }
