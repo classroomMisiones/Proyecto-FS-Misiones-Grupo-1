fdfe79e73f4ba7794378ae04281c9e7c8d3b06f3
exequieldev93 <exequiel191993@gmail.com>
2021-10-19 20:11:01 -0300

    Modificacion backend importante

--- Backend/MyMoney/MyMoney/Controllers/CarterasController.cs
+++ Backend/MyMoney/MyMoney/Controllers/CarterasController.cs
@@ -7,9 +7,11 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace MyMoney.Controllers
 {
+    [EnableCors(origins: "http://localhost:4200", headers: "*", methods: "*")]
     public class CarterasController : ApiController
     {
         string cadena = ConfigurationManager.ConnectionStrings["MiCadena"].ConnectionString;
--- src/app/Inicio/modulo-pesos/modulo-pesos.component.ts
+++ src/app/Inicio/modulo-pesos/modulo-pesos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-modulo-pesos',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./modulo-pesos.component.css']
 })
 export class ModuloPesosComponent implements OnInit {
+  listsaldo: any[] = [
+    {moneda: 'Pesos', operacion: 'Tranferencia', monto:'50', fecha: "10/12"},
+    {moneda: 'Pesos', operacion: 'Recibiste', monto:'50', fecha: "7/12"},
+    {moneda: 'Pesos', operacion: 'Ingresaste', monto:'50', fecha: "8/12"},
+  ];
 
-  constructor() { }
+  form: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.form=this.fb.group({
+      //Definimos los atributos que queremos traer
+      cvv: ['']
+    })
+  }
 
   ngOnInit(): void {
   }
 
+  
 }
