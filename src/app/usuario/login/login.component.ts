8d388ecf96efafa2adb283ad4e08d246579ec44c
Maximiliano Gamón <maximilianogamon@gmail.com>
2021-10-21 14:54:50 -0300

    Login 1.0
    Verifica el correo y la contraseña con los clientes en la base de datos.
    Almacena el cliente que hizo login en _loginService, en una variable pública, para que se use en otros componentes.
    Validaciones con Angular.
    Notificaciones.

--- src/app/usuario/login/login.component.ts
+++ src/app/usuario/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginService } from 'app/services/login.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -8,14 +11,39 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
+  idClienteLogin: number = 0;
+
   login = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.email]),
+    email: new FormControl('', [Validators.required]),
     contrasena: new FormControl('', [Validators.required, Validators.minLength(5)])
   })
 
-  constructor() { }
+  constructor(private toastr: ToastrService,
+    private _loginService: LoginService,
+    private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  loginConEmailoUsuario() {
+    const logincliente: any = {
+      email: this.login.get('email')?.value,
+      contrasena: this.login.get('contrasena')?.value
+    }
+
+    this._loginService.loginConEmailoUsuario(logincliente).subscribe(data => {
+      console.log(data);
+      if (data == 0) {
+        this.toastr.warning("Algún dato es incorrecto, pruebe nuevamente.", "Oops...", {closeButton: true})
+      } else {
+        this.toastr.success("Ha iniciado sesión exitosamente.", 
+      "Sesión Iniciada", {closeButton: true});
+      this._loginService.idClienteLogin = data;
+      console.log("El valor de idClienteLogin en el Login Service es de: " + this._loginService.idClienteLogin);
+      this.login.reset();
+      this.router.navigateByUrl('/modulopesos');
+      }
+    })
   }
 
   get email() {
